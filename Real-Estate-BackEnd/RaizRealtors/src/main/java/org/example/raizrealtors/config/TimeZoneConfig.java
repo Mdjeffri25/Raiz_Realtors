c652f831fcc8aff98702dd59a0a3bc40fc235534
mdjeffri25 <mohamedjeffri30@gmail.com>
2026-09-11 22:11:42 +0530

    Fix application timezone to IST

---
+++ Real-Estate-BackEnd/RaizRealtors/src/main/java/org/example/raizrealtors/config/TimeZoneConfig.java
@@ -0,0 +1,17 @@
+package org.example.raizrealtors.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class TimeZoneConfig {
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(
+            TimeZone.getTimeZone("Asia/Kolkata")
+        );
+    }
+}
